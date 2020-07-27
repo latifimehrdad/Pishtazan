618bbbe4cca7283b919d6d21cd704bd45c93fad8
980902 <m3.latifi@gmail.com>
2020-07-27 13:14:27 +0430

    نمایش فیلم

--- app/src/main/java/ir/bppir/pishtazan/views/fragments/MoviePlayer.java
+++ app/src/main/java/ir/bppir/pishtazan/views/fragments/MoviePlayer.java
@@ -1,17 +1,23 @@
 package ir.bppir.pishtazan.views.fragments;
 
 import android.content.pm.ActivityInfo;
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.VideoView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.databinding.DataBindingUtil;
+
+import com.universalvideoview.UniversalMediaController;
+import com.universalvideoview.UniversalVideoView;
 
 import butterknife.BindView;
 import ir.bppir.pishtazan.R;
@@ -30,6 +36,16 @@ public class MoviePlayer extends FragmentPrimary implements FragmentPrimary.GetM
     @BindView(R.id.VideoViewMovie)
     VideoView VideoViewMovie;
 
+    @BindView(R.id.universalVideoViewMovie)
+    UniversalVideoView universalVideoViewMovie;
+
+    @BindView(R.id.universalMediaController)
+    UniversalMediaController universalMediaController;
+
+    @BindView(R.id.video_layout)
+    FrameLayout video_layout;
+
+
     @Nullable
     @Override
     public View onCreateView(
@@ -43,7 +59,7 @@ public class MoviePlayer extends FragmentPrimary implements FragmentPrimary.GetM
             binding.setMovie(vm_moviePlayer);
             setView(binding.getRoot());
             movieUrl = getArguments().getString(getContext().getResources().getString(R.string.ML_MovieUrl), "");
-            getContext().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+            //getContext().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         }
         return getView();
     }//_____________________________________________________________________________________________ onCreateView
@@ -70,8 +86,58 @@ public class MoviePlayer extends FragmentPrimary implements FragmentPrimary.GetM
     private void init() {//_________________________________________________________________________ init
 
         Uri uri=Uri.parse(movieUrl);
-        VideoViewMovie.setVideoURI(uri);
-        VideoViewMovie.start();
+        universalVideoViewMovie.setMediaController(universalMediaController);
+        universalVideoViewMovie.setVideoURI(uri);
+        universalVideoViewMovie.start();
+
+
+        universalVideoViewMovie.setVideoViewCallback(new UniversalVideoView.VideoViewCallback() {
+            private boolean isFullscreen;
+
+            @Override
+            public void onScaleChange(boolean isFullscreen) {
+                this.isFullscreen = isFullscreen;
+                if (isFullscreen) {
+                    ViewGroup.LayoutParams layoutParams = video_layout.getLayoutParams();
+                    layoutParams.width = ViewGroup.LayoutParams.MATCH_PARENT;
+                    layoutParams.height = ViewGroup.LayoutParams.MATCH_PARENT;
+                    video_layout.setLayoutParams(layoutParams);
+                    //GONE the unconcerned views to leave room for video and controller
+                    //mBottomLayout.setVisibility(View.GONE);
+                } else {
+                    ViewGroup.LayoutParams layoutParams = video_layout.getLayoutParams();
+                    layoutParams.width = ViewGroup.LayoutParams.MATCH_PARENT;
+//                    layoutParams.height = this.cachedHeight;
+                    video_layout.setLayoutParams(layoutParams);
+                    //mBottomLayout.setVisibility(View.VISIBLE);
+                }
+            }
+
+            @Override
+            public void onPause(MediaPlayer mediaPlayer) { // Video pause
+                Log.i("meri", "onPause UniversalVideoView callback");
+            }
+
+            @Override
+            public void onStart(MediaPlayer mediaPlayer) { // Video start/resume to play
+                Log.i("meri", "onStart UniversalVideoView callback");
+            }
+
+            @Override
+            public void onBufferingStart(MediaPlayer mediaPlayer) {// steam start loading
+                Log.i("meri", "onBufferingStart UniversalVideoView callback");
+            }
+
+            @Override
+            public void onBufferingEnd(MediaPlayer mediaPlayer) {// steam end loading
+                Log.i("meri", "onBufferingEnd UniversalVideoView callback");
+            }
+
+        });
+
+
+//        VideoViewMovie.setVideoURI(uri);
+//        VideoViewMovie.start();
     }//_____________________________________________________________________________________________ init
 
 
